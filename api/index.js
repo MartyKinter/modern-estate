b90be0b87f97084da948ca076abd497055e749bd
MartyKinter <116516090+MartyKinter@users.noreply.github.com>
2023-12-18 13:53:40 -0500

    testing deploy

--- api/index.js
+++ api/index.js
@@ -19,6 +19,8 @@ mongoose
     console.log(err);
   });
 
+  const __directory = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -34,10 +36,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/listing', listingRoutes);
 
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__directory, '/client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__directory, 'client', 'dist', 'index.html'));
 })
 
 app.use((err, req, res, next) => {
