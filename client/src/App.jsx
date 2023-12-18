6d08b0e349e2b92b2cb9b550907d392848e12cb3
MartyKinter <116516090+MartyKinter@users.noreply.github.com>
2023-12-18 12:49:15 -0500

    fixed app.jsx to have correct about import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import About from "./pages/about";
+import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
