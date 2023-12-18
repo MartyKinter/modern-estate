eee0102d756b02dbb9a84df57465641f3977e75f
MartyKinter <116516090+MartyKinter@users.noreply.github.com>
2023-12-18 12:56:03 -0500

    edited for render deploy

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ app.use('/api/listing', listingRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
